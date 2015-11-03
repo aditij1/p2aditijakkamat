aa1ef8a1c394207f8e7c26382506cdae337ffaae
Ameya Kamat <akkamat@andrew.cmu.edu>
2015-11-03 01:20:28 -0500

    Begin implementing tribServer>

--- src/github.com/cmu440/tribbler/tribserver/tribserver_impl.go
+++ src/github.com/cmu440/tribbler/tribserver/tribserver_impl.go
@@ -2,14 +2,17 @@ package tribserver
 
 import (
 	"errors"
-
+	//"net"
+	//"net/rpc"
+	"github.com/cmu440/tribbler/libstore"
+	//"github.com/cmu440/tribbler/rpc/storagerpc"
 	"github.com/cmu440/tribbler/rpc/tribrpc"
-	"github.com/cmu440/tribbler/rpc/libstore"
-	"github.com/cmu440/tribbler/rpc/librpc"
+	//"github.com/cmu440/tribbler/rpc/librpc"
+	"github.com/cmu440/tribbler/util"
 )
 
 type tribServer struct {
-	libStore Libstore
+	libStore libstore.Libstore
 }
 
 // NewTribServer creates, starts and returns a new TribServer. masterServerHostPort
@@ -19,14 +22,14 @@ type tribServer struct {
 //
 // For hints on how to properly setup RPC, see the rpc/tribrpc package.
 func NewTribServer(masterServerHostPort, myHostPort string) (TribServer, error) {
-	libStore,err := NewLibstore(masterServerHostPort, myHostPort, LeaseMode.Never)
+	libStore,err := libstore.NewLibstore(masterServerHostPort, myHostPort, libstore.Never)
 
 	if(err != nil) {
 		return nil, err
 	}
 
 	var tribServ tribServer = tribServer{
-		libStore: libStore
+		libStore: libStore,
 	}
 
 
@@ -34,34 +37,104 @@ func NewTribServer(masterServerHostPort, myHostPort string) (TribServer, error) 
 }
 
 func (ts *tribServer) CreateUser(args *tribrpc.CreateUserArgs, reply *tribrpc.CreateUserReply) error {
-	
-	return errors.New("not implemented")
+
+	var usrID string = args.UserID
+
+	err := ts.libStore.Put(util.FormatUserKey(usrID), "exists")
+
+	if(err != nil) {
+
+		return err
+	}
+
+	//populate reply struct
+	reply.Status = tribrpc.OK
+	return nil
 }
 
 func (ts *tribServer) AddSubscription(args *tribrpc.SubscriptionArgs, reply *tribrpc.SubscriptionReply) error {
-	return errors.New("not implemented")
+	/*
+	-Check if user present
+	-Check if user being subscribed to is present
+	-append to list: sublist key formatter,
+	*/	
+
+	var thisUsrId string = args.UserID
+	var subscrId string = args.TargetUserID
+
+	_,err := ts.libStore.Get(util.FormatUserKey(thisUsrId))
+
+	if(err != nil) {
+		//user not found
+		return err
+	}
+	//add to list of subscribers
+	err = ts.libStore.AppendToList(util.FormatUserKey(thisUsrId),subscrId)
+
+	if(err != nil) {
+		return err
+	}
+
+	/* no error */
+	reply.Status = tribrpc.OK
+	return nil
 }
 
 func (ts *tribServer) RemoveSubscription(args *tribrpc.SubscriptionArgs, reply *tribrpc.SubscriptionReply) error {
+	
+	/*
+	check if both values exist, then delete the sub
+	*/
 	return errors.New("not implemented")
 }
 
 func (ts *tribServer) GetSubscriptions(args *tribrpc.GetSubscriptionsArgs, reply *tribrpc.GetSubscriptionsReply) error {
+	/*
+	formatsublistkey
+	
+	*/
 	return errors.New("not implemented")
 }
 
 func (ts *tribServer) PostTribble(args *tribrpc.PostTribbleArgs, reply *tribrpc.PostTribbleReply) error {
+	/*
+	-Check if user is present
+	-Create the tribble
+	-timestamp: go time
+	-create a tribble
+	-marshal it
+
+	-Appendto list that post key to the usrID
+	-Add to the map from post key -> marshalled tribble
+
+	*/
+
 	return errors.New("not implemented")
 }
 
+
 func (ts *tribServer) DeleteTribble(args *tribrpc.DeleteTribbleArgs, reply *tribrpc.DeleteTribbleReply) error {
+	/*
+	2 deletes:
+	-remove the tribble itself
+	-remove the postkey
+	*/
 	return errors.New("not implemented")
 }
 
 func (ts *tribServer) GetTribbles(args *tribrpc.GetTribblesArgs, reply *tribrpc.GetTribblesReply) error {
+	/*
+	-getList, formatTribListKey to get all the post keys 
+	-Slice it off at 100 
+	-reverse it or wtv (if needed)
+	-Get() with that post key, and get the marshalled tribble
+	*/
 	return errors.New("not implemented")
 }
 
 func (ts *tribServer) GetTribblesBySubscription(args *tribrpc.GetTribblesArgs, reply *tribrpc.GetTribblesReply) error {
+	/*
+	-format sublist
+	*/
 	return errors.New("not implemented")
 }
